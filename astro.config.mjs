c918b049a04cd0a304979f182e30668c72870b55
Dimitri Ntempos <ntemposd@gmail.com>
2025-10-17 16:59:13 +0300

    Enable stylesheet via config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://ntemposd.github.io', // your Pages domain
+  base: '/myastro/',                   // repo subpath  ← IMPORTANT
+  output: 'static',
   integrations: [tailwind()],
 });
